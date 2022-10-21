49df3ad5d405e9aa39a189d3e29eacb890e2041c
Ralph Ursprung <ralph.ursprung@gmail.com>
2022-10-21 10:11:31 +0200

    initial MPU6050 example

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
-//! Demonstrate the use of a blocking `Delay` using the SYST (sysclock) timer.
-//! taken directly from here: https://github.com/stm32-rs/stm32f4xx-hal/blob/master/examples/delay-syst-blinky.rs
-
+//! Demonstrate the usage of the `MPU6050` crate, based on their README and a previous blink example.
 #![deny(unsafe_code)]
 #![allow(clippy::empty_loop)]
 #![no_main]
 #![no_std]
 
-// Halt on panic
-use panic_halt as _; // panic handler
+use core::fmt::Write;
+
+use panic_halt as _; // Halt on panic
 
 use cortex_m_rt::entry;
-use stm32f4xx_hal as hal;
+use stm32f4xx_hal::{
+    i2c::Mode,
+    pac::{self},
+    prelude::*,
+    serial::config::Config,
+};
 
-use crate::hal::{pac, prelude::*};
+use mpu6050::*;
 
 #[entry]
 fn main() -> ! {
@@ -20,22 +24,61 @@ fn main() -> ! {
         pac::Peripherals::take(),
         cortex_m::peripheral::Peripherals::take(),
     ) {
-        // Set up the LED. On the Nucleo-401RE it's connected to pin PA5.
-        let gpioa = dp.GPIOA.split();
-        let mut led = gpioa.pa5.into_push_pull_output();
-
-        // Set up the system clock. We want to run at 48MHz for this one.
+        // Set up the system clock.
         let rcc = dp.RCC.constrain();
         let clocks = rcc.cfgr.sysclk(48.MHz()).freeze();
+
+        // Set up the UART pin
+        let gpioa = dp.GPIOA.split();
+        let tx_pin = gpioa.pa2.into_alternate();
+        let mut tx = dp
+            .USART2
+            .tx(
+                tx_pin,
+                Config::default()
+                    .baudrate(115200.bps())
+                    .wordlength_8()
+                    .parity_none(),
+                &clocks,
+            )
+            .unwrap();
+
+        // Set up the I2C pins
+        let gpiob = dp.GPIOB.split();
+        let scl = gpiob.pb8;
+        let sda = gpiob.pb9;
 
         // Create a delay abstraction based on SysTick
         let mut delay = cp.SYST.delay(&clocks);
 
+        let i2c = dp.I2C1.i2c(
+            (scl, sda),
+            Mode::Standard {
+                frequency: 100.kHz(),
+            },
+            &clocks,
+        );
+
+        let mut mpu = Mpu6050::new(i2c);
+        mpu.init(&mut delay).unwrap();
+
         loop {
-            // On for 1s, off for 1s.
-            led.set_high();
-            delay.delay_ms(1000_u32);
-            led.set_low();
+            // get roll and pitch estimate
+            let acc = mpu.get_acc_angles().unwrap();
+            writeln!(tx, "r/p: {:?}", acc).unwrap();
+
+            // get temp
+            let temp = mpu.get_temp().unwrap();
+            writeln!(tx, "temp: {:?}c", temp).unwrap();
+
+            // get gyro data, scaled with sensitivity
+            let gyro = mpu.get_gyro().unwrap();
+            writeln!(tx, "gyro: {:?}", gyro).unwrap();
+
+            // get accelerometer data, scaled with sensitivity
+            let acc = mpu.get_acc().unwrap();
+            writeln!(tx, "acc: {:?}", acc).unwrap();
+
             delay.delay_ms(1000_u32);
         }
     }
